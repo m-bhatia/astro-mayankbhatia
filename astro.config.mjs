af541fc98c2f6e0c715eb97640f15456b08dbb47
Mayank Bhatia <mikkubhatia@gmail.com>
2024-07-15 13:12:20 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-//   site: 'https://mayankbhatia.com',
-//   base: '/home',
-//   redirects: {
-//     'https://www.mayankbhatia.com': 'https://mayankbhatia.com',
-//     'http://www.mayankbhatia.com': 'https://mayankbhatia.com',
-//     'http://mayankbhatia.com': 'https://mayankbhatia.com'
-//   }
+  site: 'https://www.mayankbhatia.com',
+  base: '/home',
 });
